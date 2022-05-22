cea7624025d7229ef69f212925af83b8658ee5fc
JamesFalconer <Jamesfalconer007@gmail.com>
2022-05-22 12:07:15 +0200

    Update Model performance on unseen data.py

--- Model performance on unseen data.py
+++ Model performance on unseen data.py
@@ -11,15 +11,15 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 
 #change filepath to where models are stored
-filepath = "C:/users/james/Downloads/MLProj/Models/"
+filepath = "C:/users/Downloads/MLProj/Models/"
 small_model1 = load_model(
     filepath+"Model_hold_small_data2",
     custom_objects=None,
     compile=True
 )
 #edit path to where test data is stored
-input_test = np.load("C:/users/james/Downloads/MLProj/Data_test_small/x.npy")
-target_test = np.load("C:/users/james/Downloads/MLProj/Data_test_small/y.npy",allow_pickle=True)
+input_test = np.load("C:/users/Downloads/MLProj/Data_test_small/x.npy")
+target_test = np.load("C:/users/Downloads/MLProj/Data_test_small/y.npy",allow_pickle=True)
 
 target_test = LabelEncoder().fit_transform(target_test[:,0])
 
